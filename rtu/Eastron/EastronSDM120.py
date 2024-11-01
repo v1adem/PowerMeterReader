b30e6672bcef91c0ce33053c8000465b1fa432b0
v1adem <vladem255@gmail.com>
2024-11-01 10:12:21 +0200

    feat: device unificated

--- rtu/Eastron/EastronSDM120.py
+++ rtu/Eastron/EastronSDM120.py
@@ -1,14 +1,18 @@
+from rtu.Device import Device
 from rtu.SerialReaderRS485 import SerialReaderRS485
 
 
-class EastronSDM120:
-    def __init__(self, device_address, port):
-        self.device_address = device_address
-        self.device_name = "Eastron-SDM120"
-        self.port = port
-        self.reader = SerialReaderRS485(device_name=self.device_name,
+class EastronSDM120(Device):
+    def __init__(self, custom_name, device_address, port):
+        super().__init__(custom_name, device_address, "Eastron-SDM120", port, 9600, 8, "N", 1)
+        self.reader = SerialReaderRS485(device_custom_name=custom_name,
+                                        device_name=self.device_name,
                                         port=self.port,
-                                        device_address=self.device_address)
+                                        device_address=self.device_address,
+                                        baudrate=self.baudrate,
+                                        bytesize=self.bytesize,
+                                        parity=self.parity,
+                                        stopbits=self.stopbits)
 
     def read_all_properties(self):
         """
